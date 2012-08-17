f3e4496248979b8ae0e9c565a9f435680d7ea4dd
Julian Doherty <madlep@madlep-laptop.local>
2012-08-17 10:38:27 +1000

    Add bitstream vera font

---
+++ fonts.rb
@@ -0,0 +1,4 @@
+dep 'bitstream-vera.font' do
+  source 'http://ftp.gnome.org/pub/GNOME/sources/ttf-bitstream-vera/1.10/ttf-bitstream-vera-1.10.tar.gz'
+  provides %w{Vera VeraBI VeraBd VeraIt VeraMoBI VeraMoBd VeraMoIt VeraMono VeraSe VeraSeBd}
+end
