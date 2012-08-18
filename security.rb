f036ac88fb7c8fc4764b864b9c43341e62576528
richo <richo@psych0tik.net>
2012-08-18 15:35:40 +1000

    Install pwgen

--- security.rb
+++ security.rb
@@ -1,7 +1,9 @@
 dep 'security toolkit' do
   requires 'aircrack-ng.managed',
-           'nmap.managed'
+           'nmap.managed',
+           'pwgen.managed'
 end
 
 dep 'aircrack-ng.managed'
 dep 'nmap.managed'
+dep 'pwgen.managed'
