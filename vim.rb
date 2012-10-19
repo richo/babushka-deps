2683404ec3cb792dad3b42cf0514fa31e79f5911
richo <richo@psych0tik.net>
2012-10-19 11:17:31 +1100

    Elaborate hack to make MacVim inherit my shell environment

--- vim.rb
+++ vim.rb
@@ -18,3 +18,46 @@ dep "macvim.managed" do
   installs "macvim"
 end
 
+dep "vim springboards from .profile" do
+  requires "MacVim.app"
+  macvim_binary = "/Applications/MacVim.app/Contents/MacOS/MacVim"
+  # MacVim starts as a child of the login session and misses a bunch of
+  # important stuff.
+  # Install a shell script that loads my profile, then macvim
+  def is_shell_script?(path)
+    File.open(path) do |f|
+      f.read(9) == "#!/bin/sh"
+    end
+  end
+  def is_executable?(path)
+    (File.stat(path).mode & 0111) > 0
+  end
+
+  met? {
+    [
+      is_shell_script?(macvim_binary),
+      is_executable?(macvim_binary)
+    ].all?
+  }
+  meet {
+    File.rename(macvim_binary, "#{macvim_binary}.real") unless is_shell_script?("#{macvim_binary}.real")
+    File.open(macvim_binary, "w") do |f|
+      f.write <<-EOS
+#!/bin/sh
+source ~/.profile
+exec #{macvim_binary}.real
+EOS
+      f.chmod(0755)
+    end
+  }
+end
+
+dep "vim environment" do
+  on(:osx) do
+    requires "MacVim.app",
+             "vim springboards from .profile"
+  end
+  on(:ubuntu) do
+    requires "vim-full.managed"
+  end
+end
